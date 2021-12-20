085c4e26050259a9fa105e1456bc106ed2ec2eb7
adkoch15 <81789856+adkoch15@users.noreply.github.com>
2021-12-19 22:55:10 -0600

    Add files via upload

---
+++ Costume Closet/Shared/Camera.swift
@@ -0,0 +1,11 @@
+//
+//  Camera.swift
+//  Costume Closet
+//
+//  Created by Anthony Koch on 12/19/21.
+//
+
+import UIKit
+
+
+
---
+++ Costume Closet/Shared/Costume_ClosetApp.swift
@@ -0,0 +1,17 @@
+//
+//  Costume_ClosetApp.swift
+//  Shared
+//
+//  Created by Anthony Koch on 12/13/21.
+//
+
+import SwiftUI
+
+@main
+struct Costume_ClosetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
